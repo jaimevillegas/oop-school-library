2ffa6a2509b686ec913d3bd8ed41c86e53570db5
Jaime Villegas <jaimevillegas296@gmail.com>
2023-05-17 10:00:54 -0500

    Work on make associations between classes

---
+++ rental.rb
@@ -0,0 +1,12 @@
+class Rental
+  attr_accessor :date
+  def initialize(date)
+    @date = date
+
+    @person = person
+    person.add_rental(self)
+
+    @book = book
+    book.add_rental(self)
+  end
+end
