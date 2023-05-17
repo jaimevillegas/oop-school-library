53974bc66e6d7f10035bf5caa674459fb412ac3c
Jaime Villegas <jaimevillegas296@gmail.com>
2023-05-17 10:05:29 -0500

    Work on linter messages

--- student.rb
+++ student.rb
@@ -3,6 +3,7 @@ require './person'
 # Class Student that inherits from Person
 class Student < Person
   attr_accessor :classroom
+
   # Constructor that extends parent's constructor by adding @classroom and a parameter for it
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
