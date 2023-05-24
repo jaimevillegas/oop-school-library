f9ec6595b04603caf5055e08f88c1533517fda46
David Espino <davidespinog@gmail.com>
2023-05-24 11:11:34 -0600

    Add nameable test

---
+++ spec/nameable_spec.rb
@@ -0,0 +1,9 @@
+require './nameable'
+
+describe Nameable do
+  nameable = Nameable.new
+  it "Should raise an error" do 
+    expect {nameable.correct_name}.to raise_error(NotImplementedError)
+
+  end
+end
